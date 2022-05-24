0c7c7161d19848b99d6c94679586ad1f965fa4ab
Aayush Kumar <42064966+Aayush7Kumar@users.noreply.github.com>
2022-05-24 12:49:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+
+    scaler = StandardScaler()
+    final_features = scaler.fit_transform(final_features)
+
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Review score should be {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
